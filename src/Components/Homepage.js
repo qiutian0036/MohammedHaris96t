1164a1a8bd48373467e8ac27c581b201b779b1e9
qiutian0036 <a6158572@sina.com>
2021-01-03 10:47:30 +0800

    added style to page

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -5,8 +5,11 @@ const Homepage = () => {
 
   return (
     <div className="welcome-message">
-      <h1>{message} this is the Homepage for the application</h1>
-      <button className="welcome-button" onClick={() => setMessage("Welcome")}>Click Me</button>
+      <h1>{message}, this is the Homepage for the application</h1>
+      <button className="welcome-button" onClick={() => setMessage("Welcome")}>
+        Click Me
+      </button>
+      <img src="https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
     </div>
   );
 };
