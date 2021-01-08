5b155c49f59ce3672fb84065945a28ea6db8488b
qiutian0036 <a6158572@sina.com>
2021-01-09 03:36:23 +0800

    new changes

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -11,6 +11,7 @@ const Homepage = () => {
         Click Me
       </Button>
       <img src="https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
+      <img src="https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
     </div>
   );
 };
