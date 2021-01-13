506ebcc5e5d7d28e41350048880891e554173e61
qiutian0036 <a6158572@sina.com>
2021-01-13 10:08:26 +0800

    new changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <Navbar />
       <Homepage /> <Homepage /> <Homepage /> <Homepage />
+      <Homepage /> <Homepage /> <Homepage /> <Homepage />
+      <Homepage /> <Homepage /> <Homepage /> <Homepage /> <Homepage />{" "}
+      <Homepage /> <Homepage /> <Homepage />
     </div>
   );
 }
