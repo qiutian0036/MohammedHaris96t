5b4dacd27344e7a8e18a58437681165e9c84c813
qiutian0036 <a6158572@sina.com>
2021-01-19 16:00:22 +0800

    carousel added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import "./App.css";
 import Homepage from "./Components/Homepage";
 import Navbar from "./Components/Navbar";
+import Carouselmain from "./Components/Carouselmain";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Homepage /> <Homepage /> <Homepage /> <Homepage />
-      <Homepage /> <Homepage /> <Homepage /> <Homepage />
-      <Homepage /> <Homepage /> <Homepage /> <Homepage /> <Homepage />{" "}
-      <Homepage /> <Homepage /> <Homepage /> <Homepage /> <Homepage />{" "}
-      <Homepage /> <Homepage /> <Homepage /> <Homepage />
-      <Homepage /> <Homepage /> <Homepage />
+      <Homepage />
+      <Carouselmain />
     </div>
   );
 }
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -10,8 +10,7 @@ const Homepage = () => {
       <Button className="welcome-button" onClick={() => setMessage("Welcome")}>
         Click Me
       </Button>
-      <img src="https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
-      <img src="https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
+    
     </div>
   );
 };
