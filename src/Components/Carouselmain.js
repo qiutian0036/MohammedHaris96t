5c7afe17e1bf23c80369555e20890ea76555b86d
qiutian0036 <a6158572@sina.com>
2021-01-31 16:31:29 +0800

    new changes

--- src/Components/Carouselmain.js
+++ src/Components/Carouselmain.js
@@ -1,23 +1,30 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
 
 const Carouselmain = () => {
   return (
-    <div>
-      <Carousel>
-        <Carousel.Item interval={1000}>
-          <img
-            className="d-block w-100"
-            src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>this is a test image.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </div>
+    <Container>
+      <row>
+        <col md={6}>
+          <div>
+            <Carousel>
+              <Carousel.Item interval={1000}>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>this is a test image.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        </col>
+      </row>
+    </Container>
   );
 };
 
