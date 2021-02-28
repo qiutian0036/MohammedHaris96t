a6ea15b3baa75f40e0b244c05f12d2115010fdc9
qiutian0036 <a6158572@sina.com>
2021-03-01 05:49:47 +0800

    Changes

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap";
 const Homepage = () => {
   return (
     <Container>
-      <h1>Homepage Component</h1>
+      <h1>Homepage</h1>
     </Container>
   );
 };
