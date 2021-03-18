325cadb0724ce888b0be6b37c2ea801daf1c4cb3
qiutian0036 <a6158572@sina.com>
2021-03-18 16:52:21 +0800

    made changes in the files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import Homepage from "./Components/Homepage";
 import Navbar from "./Components/Navbar";
-import Carouselmain from "./Components/Carouselmain";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Homepage />
-      {/* <Carouselmain /> */}
     </div>
   );
 }
