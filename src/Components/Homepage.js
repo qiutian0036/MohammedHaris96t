83278cb9099203b9f7fcc996b2d3b735cbcebc61
qiutian0036 <a6158572@sina.com>
2021-04-07 09:49:55 +0800

    new change

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -3,7 +3,7 @@ import AuthorImage from "./AuthorImage";
 const Homepage = () => {
   return (
     <div className="homepage-container ">
-      <h1>This is a container for the blog page</h1>
+      <h1>This is a container for the blog page....</h1>
       <AuthorImage />
     </div>
   );
